f8be6ec8662946f585dc8e0074271ce39300c46a
=johnChangliang <john_changliang@163.com>
2016-11-21 16:17:49 +0800

    修改系统分页功能，基于pageCount*15的规则进行分页

--- src/components/HomeWorkTableComponents.js
+++ src/components/HomeWorkTableComponents.js
@@ -24,6 +24,8 @@ const HomeWorkTableComponents = React.createClass({
       selectedRowKeys: [],  // Check here to configure the default column
       loading: false,
       count:0,
+      totalCount:0,
+      currentPage:1,
       currentView:'homeWorkList'
     };
   },
@@ -126,7 +128,6 @@ const HomeWorkTableComponents = React.createClass({
         console.log("getSubjectDataMSG:"+ret.msg);
         subjectList=new Array();
         var response = ret.response;
-        var count=0;
         response.forEach(function (e) {
           console.log("getDoneHomeworkList:"+e);
           var colCid = e.colCid;
@@ -146,9 +147,9 @@ const HomeWorkTableComponents = React.createClass({
               useDate:subTable.getLocalTime(useDate),
               subjectOpt:subjectOpt,
           });
-          count++;
         });
-        console.log("count:"+count);
+        var pager = ret.pager;
+        subTable.setState({totalCount:parseInt(pager.pageCount)*15});
         subTable.setState({currentView:'homeWorkList'});
       },
       onError : function(error) {
@@ -197,12 +198,22 @@ const HomeWorkTableComponents = React.createClass({
             subjectScore:score,
           });
         });
+        var pager = ret.pager;
+        subTable.setState({totalCount:parseInt(pager.pageCount)*15});
         subTable.setState({currentView:'subjectList'});
       },
       onError : function(error) {
         alert(error);
       }
 
+    });
+  },
+
+  pageOnChange(pageNo) {
+    console.log(pageNo);
+    subTable.getDoneHomeworkList("23836",pageNo);
+    this.setState({
+      currentPage: pageNo,
     });
   },
 
@@ -219,7 +230,7 @@ const HomeWorkTableComponents = React.createClass({
     const hasSelected = selectedRowKeys.length > 0;
     return (
       <div >
-        <Table rowSelection={rowSelection} columns={columns}  dataSource={data} pagination={{ pageSize: 10 }} scroll={{ y: 400}}/>
+        <Table rowSelection={rowSelection} columns={columns}  dataSource={data} pagination={{ total:subTable.state.totalCount,pageSize: 15,onChange:subTable.pageOnChange }} scroll={{ y: 400}}/>
       </div>
     );
   },
