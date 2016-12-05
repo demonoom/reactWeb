5a5b3b258802c666058047954904100b8e397f82
=johnChangliang <john_changliang@163.com>
2016-12-05 15:41:02 +0800

    修改知识点导航，记录用户的访问位置

--- src/components/FloatButton.js
+++ src/components/FloatButton.js
@@ -5,6 +5,13 @@ import { Popover, Affix, Button } from 'antd';
 const FloatButton = React.createClass({
 
     logOut(){
+        // alert(sessionStorage.getItem("openKeysStr"));
+        var openKeysStr = sessionStorage.getItem("openKeysStr");
+        if(openKeysStr!=null && openKeysStr!=""){
+            //已有访问记录，本地移除后，保存到数据库
+            sessionStorage.removeItem("openKeysStr");
+            //toDoList  保存到数据库
+        }
         if(confirm("您确定退出登录么?")){
             sessionStorage.removeItem("ident");
             location.hash="Login";
