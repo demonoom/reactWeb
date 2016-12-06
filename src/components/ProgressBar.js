9d028e88bb3a0691268e8a26a16d8e01982c63ab
luble <282558417@qq.com>
2016-12-06 15:55:03 +0800

    progress_bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,8 +5,8 @@ const ProgressBar = React.createClass({
 
     render() {
         return (
-            <div style={{backgroundColor:'gray',heigth:8,width:'100%'}}>
-                <div id="pro" style={{width:'20%',height:5,backgroundColor:'green'}}></div>
+            <div className="progress_bar">
+                <div id="pro" className="progress_thumb" style={{width:'60%'}}></div>
             </div>
         );
     }
