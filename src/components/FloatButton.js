d460b947a2577b7d4df722e583406a9e316f0382
=johnChangliang <john_changliang@163.com>
2016-12-15 14:58:35 +0800

    增加用户访问位置记录功能

--- src/components/FloatButton.js
+++ src/components/FloatButton.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { Popover, Affix, Button } from 'antd';
+import { doWebService } from '../WebServiceHelper';
 
 
 const FloatButton = React.createClass({
@@ -7,15 +8,39 @@ const FloatButton = React.createClass({
     logOut(){
         // alert(sessionStorage.getItem("openKeysStr"));
         var openKeysStr = sessionStorage.getItem("openKeysStr");
-        if(openKeysStr!=null && openKeysStr!=""){
-            //已有访问记录，本地移除后，保存到数据库
-            sessionStorage.removeItem("openKeysStr");
-            //toDoList  保存到数据库
-        }
         if(confirm("您确定退出登录么?")){
-            sessionStorage.removeItem("ident");
-            location.hash="Login";
+            if(openKeysStr!=null && openKeysStr!=""){
+                //已有访问记录，本地移除后，保存到数据库
+                var userId = sessionStorage.getItem("ident");
+                this.saveHistoryAccessPointId(userId,openKeysStr);
+            }else{
+                location.hash="Login";
+            }
         }
+    },
+
+    saveHistoryAccessPointId(userId,pointId){
+        var param = {
+            "method":'saveHistoryAccessPointId',
+            "userId":userId,
+            "pointId":pointId
+        };
+        doWebService(JSON.stringify(param), {
+            onResponse : function(ret) {
+                console.log(ret.msg);
+                if(ret.msg=="调用成功" && ret.response==true){
+                    console.log("保存此用户上次访问的知识点id成功");
+                    sessionStorage.removeItem("openKeysStr");
+                    sessionStorage.removeItem("ident");
+                }else{
+                    console.log("保存此用户上次访问的知识点id失败");
+                }
+                location.hash="Login";
+            },
+            onError : function(error) {
+                console.log(error);
+            }
+        });
     },
 
     render() {
