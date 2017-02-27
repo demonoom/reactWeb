c9d0b0290bf04c5578801c6801dd73cd8df953fa
=johnChangliang <john_changliang@163.com>
2017-02-27 18:05:28 +0800

    在页面头部增加帮助手册的入口，样式待优化

--- src/components/HeaderComponents.js
+++ src/components/HeaderComponents.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Button} from 'antd';
 import MaaeeLogo from './MaaeeLogo';
 import SearchInput from './SearchInput';
 import ProgressBar from '../components/ProgressBar';
 
-const HeaderComponents = (props) => {
-    return (
-        <div>
-            <MaaeeLogo/>
+var floatButton;
+const HeaderComponents = React.createClass({
 
-            {/*<Button icon="cloud-upload-o" className="colud_bnt"/>
-            <SearchInput placeholder="请输入关键字搜索"onSearch={value => console.log(value)} style={{ width:300 }} />*/}
-            <ProgressBar style={{valign:'bottom'}}></ProgressBar>
-        </div>
-    );
-};
+    redirectHelpPage(){
+        window.open("http://www.baidu.com","_blank");
+    },
 
-HeaderComponents.propTypes = {
-};
+    render() {
+        return (
+            <div>
+                <MaaeeLogo/>
 
+                {/*<Button icon="cloud-upload-o" className="colud_bnt"/>
+                 <SearchInput placeholder="请输入关键字搜索"onSearch={value => console.log(value)} style={{ width:300 }} />*/}
+                <ProgressBar style={{valign:'bottom'}}></ProgressBar>
+                <Button icon="book" onClick={this.redirectHelpPage} className="colud_bnt"/>
+            </div>
+        );
+    }
+
+});
 export default HeaderComponents;
+
+
+
