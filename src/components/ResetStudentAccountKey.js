9f754253b72f7707e5f2008347164b0bb6f21dc7
luble <282558417@qq.com>
2017-04-11 18:27:33 +0800

    mima

--- src/components/ResetStudentAccountKey.js
+++ src/components/ResetStudentAccountKey.js
@@ -78,11 +78,13 @@ class ResetStudentAccountKey extends React.Component {
 
         if (this.state.toUserAccount) {
             this.templatehtml = <div id="template">
-                <div class="ques_list_new">
-                    <img   src={this.state.toUserAccountData.avatar} width="40dp" height="40dp" />
-                    <span  class="user_name_003">{this.state.toUserAccountData.userName}</span>
-                    <Button type="primary"  onClick={this.resetUserPassword } >重置密码</Button>
+			<div className="ques_list_new">
+                <div className="password_cow ant-row ant-form-item">
+                    <div className="ant-form-item-control_2 attention_img"><img  src={this.state.toUserAccountData.avatar} width="40dp" height="40dp" /></div>
+                    <div className="ant-form-item-control_2">{this.state.toUserAccountData.userName}</div>
+                    <div className="ant-modal-footer"><Button type="primary" className="password_btn" onClick={this.resetUserPassword } >重置密码</Button></div>
                 </div>
+				</div>
             </div>;
         }
 
@@ -99,16 +101,13 @@ class ResetStudentAccountKey extends React.Component {
                     <Breadcrumb.Item href="#/MainLayout">找回学生密码</Breadcrumb.Item>
                 </Breadcrumb>
                 <Row style={{height: 100}}/>
-                <Row style={{height: 100}}>
-                    <Col xs={{span: 2, offset: 1}} lg={{span: 6, offset: 2}}></Col>
-                    <Col xs={{span: 12, offset: 1}} lg={{span: 6, offset: 2}}>
-                        <span class="ant-input-wrapper"><input class="ant-input" ref="input1" type="text" placeholder={this.state.placeholder} /></span>
-                    </Col>
-                    <Col xs={{span: 4, offset: 1}} lg={{span: 6, offset: 2}}>
-                        <Button type="primary" icon="search" onClick={this.searhAccess}>Search</Button>
-                    </Col>
-                    <Col xs={{span: 2, offset: 1}} lg={{span: 6, offset: 2}}></Col>
-                </Row>
+                 <div className="ques_list_new">
+                        <div className="password_cow ant-row ant-form-item">
+						    <div className="ant-col-6 right_look">查找账号：</div>
+							<div className="ant-col-14"><input className="ant-input" ref="input1" type="text" placeholder={this.state.placeholder} /></div>
+						</div>
+                        <div className="ant-modal-footer"><Button type="primary" icon="search" onClick={this.searhAccess}>Search</Button></div>
+                </div>
                 <Row>
                     <Col span={3}></Col>
                     <Col span={18}>
