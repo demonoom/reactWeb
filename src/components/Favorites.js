e5f0bb56ddb486aec7c09cdb215091ee1ca4ea43
dapeng <madapeng@madapeng-pc>
2017-04-20 11:02:05 +0800

    修改取消收藏的最后一条数据无法刷新页面；修复查看这个中心跳转无法准确到达。

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,7 +1,7 @@
 /**
  * Created by madapeng on 17-4-5.
  */
-import {Tabs, Breadcrumb, Icon,message,Pagination} from 'antd';
+import {Tabs, Breadcrumb, Icon, message, Pagination} from 'antd';
 import React from 'react';
 import FavItem from './FavoriteItem';
 import SubjectItem from './FavoriteSubjectItems';
@@ -24,11 +24,11 @@ class Favorites extends React.Component {
             data: [],
             activeKey: '1',
             // 默认数据
-            other: {type: 0, pageNo: 1, total:10, data: null},
-            subject: {type: 1, pageNo: 1,total:10, data: null, activeKey: '1'},
-            weike: {type: 2, pageNo: 1,total:10, data: null},
-            jiangyi: {type: 3, pageNo: 1,total:10, data: null},
-            shipin: {type: 4, pageNo: 1,total:10, data: null}
+            other: {type: 0, pageNo: 1, total: 10, data: null},
+            subject: {type: 1, pageNo: 1, total: 10, data: null, activeKey: '1'},
+            weike: {type: 2, pageNo: 1, total: 10, data: null},
+            jiangyi: {type: 3, pageNo: 1, total: 10, data: null},
+            shipin: {type: 4, pageNo: 1, total: 10, data: null}
 
         };
 
@@ -61,13 +61,12 @@ class Favorites extends React.Component {
                     message.error(res.msg);
                     return;
                 }
+                if (!res.pager) res.pager = {};
+                args.data = res.response || [];
+                args.pageNo = res.pager.pageNo || 0;
+                args.total = res.pager.pageCount || 0;
+                if (fn) fn.call(_this, args);
 
-                if (res.pager.rsCount) {
-                    args.data = res.response || [];
-                    args.pageNo = res.pager.pageNo || 0;
-                    args.total = res.pager.pageCount || 0;
-                    if (fn) fn.call(_this, args);
-                }
             },
             onError: function (error) {
                 message.error(error);
@@ -78,19 +77,19 @@ class Favorites extends React.Component {
     changeState(obj) {
         switch (parseInt(obj.type)) {
             case 0:
-                this.setState({'other': obj, type:obj.type,total:obj.total,pageNo:obj.pageNo });
+                this.setState({'other': obj, type: obj.type, total: obj.total, pageNo: obj.pageNo});
                 break;
             case 1:
-                this.setState({'subject': obj, type:obj.type,total:obj.total,pageNo:obj.pageNo });
+                this.setState({'subject': obj, type: obj.type, total: obj.total, pageNo: obj.pageNo});
                 break;
             case 2:
-                this.setState({'weike': obj, type:obj.type,total:obj.total,pageNo:obj.pageNo });
+                this.setState({'weike': obj, type: obj.type, total: obj.total, pageNo: obj.pageNo});
                 break;
             case 3:
-                this.setState({'jiangyi': obj, type:obj.type,total:obj.total,pageNo:obj.pageNo });
+                this.setState({'jiangyi': obj, type: obj.type, total: obj.total, pageNo: obj.pageNo});
                 break;
             case 4:
-                this.setState({'shipin': obj, type:obj.type,total:obj.total,pageNo:obj.pageNo });
+                this.setState({'shipin': obj, type: obj.type, total: obj.total, pageNo: obj.pageNo});
                 break;
         }
     }
@@ -101,7 +100,7 @@ class Favorites extends React.Component {
     }
 
 // tab切换
-    tabClick(type,pageNo) {
+    tabClick(type, pageNo) {
         let ref = this.FAVTYPE[type][1];
         var param = {
             method: this.state.method,
@@ -113,8 +112,8 @@ class Favorites extends React.Component {
     }
 
     // 翻页
-    pageChange(pageNo){
-       this.tabClick(this.state.type,pageNo);
+    pageChange(pageNo) {
+        this.tabClick(this.state.type, pageNo);
 
     }
 
@@ -131,10 +130,10 @@ class Favorites extends React.Component {
         doWebService(JSON.stringify(args), {
             onResponse: function (res) {
 
-                message.info("Cancel successfed!");
-                if (!!fn){
+                message.info("取消成功！");
+                if (!!fn) {
                     fn(_self.props.param.type);
-                }else{
+                } else {
                     _self.upgradeCurrent(_self.props.param.type);
                 }
             },
@@ -146,12 +145,11 @@ class Favorites extends React.Component {
     }
 
 
-
-    getBreadCrumb(){
+    getBreadCrumb() {
         let tag;
-        this.props.breadcrumbVisible === undefined ? tag= true : tag=this.props.breadcrumbVisible;
-        if(tag ){
-            this.breadcrumb = <Breadcrumb separator=">"  >
+        this.props.breadcrumbVisible === undefined ? tag = true : tag = this.props.breadcrumbVisible;
+        if (tag) {
+            this.breadcrumb = <Breadcrumb separator=">">
                 <Breadcrumb.Item><Icon type="home"/></Breadcrumb.Item>
                 <Breadcrumb.Item href="#/MainLayout">个人中心</Breadcrumb.Item>
                 <Breadcrumb.Item href="#/MainLayout">我的收藏</Breadcrumb.Item>
@@ -168,23 +166,32 @@ class Favorites extends React.Component {
                 <Tabs onTabClick={this.tabClick.bind(this)} defaultActiveKey={this.state.activeKey}>
                     {/*题目*/}
                     <TabPane tab={this.FAVTYPE[1][2]} key='1' className="topics_rela">
-                        <SubjectItem param={this.state.subject}   onCancelfavrite={this.cancelFav} upgradeData={this.upgradeCurrent.bind(this)}/>
+                        <SubjectItem param={this.state.subject} onCancelfavrite={this.cancelFav}
+                                     upgradeData={this.upgradeCurrent.bind(this)}/>
                     </TabPane>
                     {/*微课*/}
                     <TabPane tab={this.FAVTYPE[2][2]} key='2' className="topics_rela">
-                        <FavItem param={this.state.weike} onPreview={this.props.onPreview} onCancelfavrite={this.cancelFav} upgradeData={this.upgradeCurrent.bind(this)} pageChange={this.pageChange}/>
+                        <FavItem param={this.state.weike} onPreview={this.props.onPreview}
+                                 onCancelfavrite={this.cancelFav} upgradeData={this.upgradeCurrent.bind(this)}
+                                 pageChange={this.pageChange}/>
                     </TabPane>
                     {/*讲义*/}
                     <TabPane tab={this.FAVTYPE[3][2]} key='3' className="topics_rela">
-                        <FavItem param={this.state.jiangyi} onPreview={this.props.onPreview} onCancelfavrite={this.cancelFav} upgradeData={this.upgradeCurrent.bind(this)}  pageChange={this.pageChange}/>
+                        <FavItem param={this.state.jiangyi} onPreview={this.props.onPreview}
+                                 onCancelfavrite={this.cancelFav} upgradeData={this.upgradeCurrent.bind(this)}
+                                 pageChange={this.pageChange}/>
                     </TabPane>
                     {/*我的直播课*/}
                     <TabPane tab={this.FAVTYPE[4][2]} key='4' className="topics_rela">
-                        <ShippinItem param={this.state.shipin} onPreview={this.props.onPreview} onCancelfavrite={this.cancelFav} upgradeData={this.upgradeCurrent.bind(this)}  pageChange={this.pageChange}/>
+                        <ShippinItem param={this.state.shipin} onPreview={this.props.onPreview}
+                                     onCancelfavrite={this.cancelFav} upgradeData={this.upgradeCurrent.bind(this)}
+                                     pageChange={this.pageChange}/>
                     </TabPane>
                     {/*其他*/}
                     <TabPane tab={this.FAVTYPE[0][2]} key='0' className="topics_rela">
-                        <OtherItem param={this.state.other} onPreview={this.props.onPreview} onCancelfavrite={this.cancelFav} upgradeData={this.upgradeCurrent.bind(this)}  pageChange={this.pageChange}/>
+                        <OtherItem param={this.state.other} onPreview={this.props.onPreview}
+                                   onCancelfavrite={this.cancelFav} upgradeData={this.upgradeCurrent.bind(this)}
+                                   pageChange={this.pageChange}/>
                     </TabPane>
                 </Tabs>
             </div>
