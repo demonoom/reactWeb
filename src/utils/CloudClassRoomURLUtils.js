3a61c1946c8eceb9e68c0a77155e9cb2553f440a
noom <noom0130@foxmail.com>
2017-10-18 20:11:01 +0800

    .

--- src/utils/CloudClassRoomURLUtils.js
+++ src/utils/CloudClassRoomURLUtils.js
@@ -1,7 +1,7 @@
 /**
  * Created by devnote on 16-12-2.
  */
-var isDebug=true;
+var isDebug=false;
 var domain=isDebug?"192.168.2.105:8888":"www.maaee.com";
 // var domain=isDebug?"172.20.10.10:8080":"www.maaee.com";
 var liveDomain=isDebug?"192.168.1.34:8080":"www.maaee.com";
