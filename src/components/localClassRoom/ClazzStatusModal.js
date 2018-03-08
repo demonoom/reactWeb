1d071dd257b02f9581044f16f2a47432d3fe8280
luble <282558417@qq.com>
2018-03-08 19:53:45 +0800

    -

--- src/components/localClassRoom/ClazzStatusModal.js
+++ src/components/localClassRoom/ClazzStatusModal.js
@@ -56,7 +56,7 @@ class ClazzStatusModal extends React.Component {
                    onCancel={this.ClazzStatusModalHandleCancel}
                    transitionName=""  //禁用modal的动画效果
                    maskClosable={false} //设置不允许点击蒙层关闭
-                   width={800}
+                   width={500}
                    footer={null}
             >
                 <Row className="modal_flex">
