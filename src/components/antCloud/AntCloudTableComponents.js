08b152f0fff6d63b1d4cf2b41348769d37219f95
luble <282558417@qq.com>
2018-04-11 13:53:44 +0800

    -

--- src/components/antCloud/AntCloudTableComponents.js
+++ src/components/antCloud/AntCloudTableComponents.js
@@ -754,7 +754,7 @@ pageNo   --- 页码，-1取全部
                         deleteButton = "";
                         moveButton = "";
                     }
-                    var saveButton = <Button type="button" value={key} text={key}
+                    var saveButton = <Button title="保存至我的蚁盘" type="button" value={key} text={key}
                                              onClick={cloudTable.getCloudFileSave.bind(cloudTable, e)}
                                              icon="save"></Button>;
                 }
