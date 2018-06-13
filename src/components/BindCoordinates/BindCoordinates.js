b2a08b8790c343b0f08680d7110c7d7416b4aa43
luble <282558417@qq.com>
2018-06-13 15:09:14 +0800

    -

--- src/components/BindCoordinates/BindCoordinates.js
+++ src/components/BindCoordinates/BindCoordinates.js
@@ -272,13 +272,8 @@ const BindCoordinates = React.createClass({
         canvas = document.getElementById('schoolMap');
         context = canvas.getContext('2d');
         canvas.width = document.getElementsByClassName('bindCoordinates_cont')[0].offsetWidth - 240;
-<<<<<<< HEAD
         canvas.height = document.getElementsByClassName('bindCoordinates_cont')[0].offsetHeight - 37;
         context.font = '400 20px 微软雅黑'
-=======
-        canvas.height = document.getElementsByClassName('bindCoordinates_cont')[0].offsetHeight - 10;
-        context.font = '900 20px 微软雅黑'
->>>>>>> 2f001ef556a752522b3a6ebb994c4a7645b48a25
         context.fillStyle = '#ffff00'
     },
 
