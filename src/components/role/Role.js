fa435a55484502a9e32b4c648189fe886398f967
noom <noom0130@foxmail.com>
2018-06-26 14:43:03 +0800

    .~

--- src/components/role/Role.js
+++ src/components/role/Role.js
@@ -1,24 +1,12 @@
-import React, { PropTypes } from 'react';
-import { isEmpty } from '../../utils/utils';
-import { Table, Icon, Button, Breadcrumb, message, Modal } from 'antd';
+import React, {PropTypes} from 'react';
+import {isEmpty} from '../../utils/utils';
+import {Table, Icon, Button, Breadcrumb, message, Modal} from 'antd';
 import AddRoleMemberModal from './AddRoleMemberModal';
 import EditRoleModal from './EditRoleModal'
 import ConfirmModal from '../ConfirmModal'
-import { doWebService } from '../../WebServiceHelper';
+import {doWebService} from '../../WebServiceHelper';
 
 const confirm = Modal.confirm;
-
-const columns = [{
-    title: '姓名',
-    dataIndex: 'name',
-    className: 'dold_text'
-}, /*{
-    title: '部门',
-    dataIndex: 'group',
-},*/ {
-    title: '手机号',
-    dataIndex: 'phone',
-}];
 
 const Role = React.createClass({
 
@@ -38,28 +26,27 @@ const Role = React.createClass({
     },
 
     componentWillReceiveProps(nextProps) {
-
         if (isEmpty(nextProps.roleGroupClick) == false) {
             var selectedMessage = nextProps.roleGroupClick;
             var arr = selectedMessage.split(',');
-            this.setState({ roleId: arr[0], roleName: arr[1] });
+            this.setState({roleId: arr[0], roleName: arr[1], roleArr: arr});
             this.ajaxData(arr[0]);
             if (arr[2] != 0) {
-                this.setState({ disabled: true })
+                this.setState({disabled: true})
             } else {
-                this.setState({ disabled: false })
+                this.setState({disabled: false})
             }
         } else {
             if (isEmpty(nextProps.sendDefaultSelected) == false) {
                 var sendDefaultSelected = nextProps.sendDefaultSelected;
                 var arr = sendDefaultSelected.split('#');
-                this.setState({ roleId: arr[0], roleName: arr[1] });
+                this.setState({roleId: arr[0], roleName: arr[1]});
                 this.ajaxData(arr[0]);
             }
         }
         if (isEmpty(nextProps.RoleGroupName) == false) {
             var arr = nextProps.RoleGroupName.split('#');
-            this.setState({ papaName: arr[1] });
+            this.setState({papaName: arr[1]});
         }
         // if(isEmpty(selectedMessage)==false){
         //     if(selectedMessage.indexOf(',') !== -1){
@@ -110,8 +97,8 @@ const Role = React.createClass({
     drawTable(data) {
         var mermberNum = data.length;
         var _this = this;
-        _this.setState({ deleteData: data });
-        _this.setState({ mermberNum: mermberNum });
+        _this.setState({deleteData: data});
+        _this.setState({mermberNum: mermberNum});
         var mesData = [];
         data.forEach(function (v, i) {
             var person = {
@@ -122,11 +109,11 @@ const Role = React.createClass({
             }
             mesData.push(person);
         });
-        _this.setState({ mesData, selectedRowKeys: [] });
+        _this.setState({mesData, selectedRowKeys: []});
     },
 
     addRoleMember() {
-        this.setState({ addRoleModalIsShow: true });
+        this.setState({addRoleModalIsShow: true});
     },
 
     /**
@@ -134,32 +121,31 @@ const Role = React.createClass({
      */
     addRoleComplete() {
         this.ajaxData(this.state.roleId);
-        this.setState({ "addRoleModalIsShow": false });
+        this.setState({"addRoleModalIsShow": false});
     },
     /**
      * 添加成员时取消和关闭的回调
      */
     closeAddModel() {
-        this.setState({ "addRoleModalIsShow": false });
+        this.setState({"addRoleModalIsShow": false});
     },
 
     editRole() {
-        this.setState({ editRoleModalIsShow: true });
+        this.setState({editRoleModalIsShow: true});
     },
 
     /**
      * 编辑角色时取消和关闭时候的回调
      */
     closeModel() {
-        this.setState({ "editRoleModalIsShow": false });
-        console.log("cancle");
+        this.setState({"editRoleModalIsShow": false});
     },
 
     closeConfirmModal() {
-        this.setState({ changeConfirmModalVisible: false })
+        this.setState({changeConfirmModalVisible: false})
     },
     deleteModal() {
-        this.setState({ changeConfirmModalVisible: false });
+        this.setState({changeConfirmModalVisible: false});
         var _this = this;
         var selectedMem = _this.state.selectedRowKeys;
         var userIds = '';
@@ -194,7 +180,7 @@ const Role = React.createClass({
         //将编辑后的名字同步至左侧
         this.props.onEditComplete(roleName, roleId);
         //设置编辑角色Modal的显示状态为false，不再显示
-        this.setState({ "roleName": roleName, "editRoleModalIsShow": false });
+        this.setState({"roleName": roleName, "editRoleModalIsShow": false});
     },
 
     /**
@@ -211,11 +197,11 @@ const Role = React.createClass({
      * @param selectedRowKeys
      */
     onSelectChange(selectedRowKeys) {
-        this.setState({ selectedRowKeys });
+        this.setState({selectedRowKeys});
     },
 
     showConfirm() {
-        this.setState({changeConfirmModalVisible:true})
+        this.setState({changeConfirmModalVisible: true})
     },
 
     /**
@@ -228,6 +214,46 @@ const Role = React.createClass({
             onChange: this.onSelectChange,
         };
         const hasSelected = this.state.selectedRowKeys.length > 0;
+
+        var columns = [{
+            title: '姓名',
+            dataIndex: 'name',
+            className: 'dold_text'
+        }, {
+            title: '手机号',
+            dataIndex: 'phone',
+        }];
+
+        var roleArr = this.state.roleArr;
+        if (isEmpty(roleArr) == false) {
+            if (roleArr[2] != 0 && roleArr[1] == '班主任') {
+                columns = [{
+                    title: '姓名',
+                    dataIndex: 'name',
+                    className: 'dold_text'
+                }, {
+                    title: '手机号',
+                    dataIndex: 'phone',
+                }, {
+                    title: '所在班级',
+                    dataIndex: 'clazz',
+                }];
+            } else if (roleArr[2] != 0 && roleArr[1] == '年级主任') {
+                columns = [{
+                    title: '姓名',
+                    dataIndex: 'name',
+                    className: 'dold_text'
+                }, {
+                    title: '手机号',
+                    dataIndex: 'phone',
+                }, {
+                    title: '所在年级',
+                    dataIndex: 'grade',
+                }];
+            }
+        }
+        console.log(columns);
+
         return (
             <div className="schoolgroup">
                 <div className="schoolgroup_title">
@@ -235,7 +261,7 @@ const Role = React.createClass({
                     <span className="schoolgroup_people modal_course">(<span>{this.state.mermberNum}</span>人)</span>
                     <span>
                         <Button className="schoolgroup_btn_gray_6 schoolgroup_btn_left schoolgroup_btn"
-                            onClick={this.editRole} disabled={this.state.disabled}>编辑</Button>
+                                onClick={this.editRole} disabled={this.state.disabled}>编辑</Button>
                     </span>
                 </div>
 
@@ -243,34 +269,34 @@ const Role = React.createClass({
                     <div className="framework_btn_bg">
                         <span>
                             <Button className="schoolgroup_btn_blue schoolgroup_btn"
-                                onClick={this.addRoleMember}>添加成员</Button>
+                                    onClick={this.addRoleMember}>添加成员</Button>
                         </span>
                         <Button disabled={!hasSelected}
-                            className="calmBorderRadius schoolgroup_btn_red schoolgroup_btn_left schoolgroup_btn"
-                            onClick={this.showConfirm}>批量删除</Button>
-                        <span className="password_ts" style={{ marginLeft: 8 }}>
+                                className="calmBorderRadius schoolgroup_btn_red schoolgroup_btn_left schoolgroup_btn"
+                                onClick={this.showConfirm}>批量删除</Button>
+                        <span className="password_ts" style={{marginLeft: 8}}>
                             {hasSelected ? `选中 ${this.state.selectedRowKeys.length} 条记录` : ''}</span>
                     </div>
                     <Table className="framework_user" rowSelection={rowSelection} columns={columns}
-                        dataSource={this.state.mesData}
-                        pagination={false}
+                           dataSource={this.state.mesData}
+                           pagination={false}
                     />
                 </div>
 
                 <AddRoleMemberModal isShow={this.state.addRoleModalIsShow} parentRole={this.state.parentRole}
-                    callbackParent={this.listStructures}
-                    roleId={this.state.roleId}
-                    addRoleComplete={this.addRoleComplete}
-                    closeAddModel={this.closeAddModel}
-                    addedMemberData={this.state.mesData}
+                                    callbackParent={this.listStructures}
+                                    roleId={this.state.roleId}
+                                    addRoleComplete={this.addRoleComplete}
+                                    closeAddModel={this.closeAddModel}
+                                    addedMemberData={this.state.mesData}
                 ></AddRoleMemberModal>
 
                 <EditRoleModal isShow={this.state.editRoleModalIsShow} roleId={this.state.roleId}
-                    roleName={this.state.roleName}
-                    onEditComplete={this.editRoleComplete}
-                    onDelComplete={this.onDelComplete}
-                    closeModel={this.closeModel}
-                    papaName={this.state.papaName}
+                               roleName={this.state.roleName}
+                               onEditComplete={this.editRoleComplete}
+                               onDelComplete={this.onDelComplete}
+                               closeModel={this.closeModel}
+                               papaName={this.state.papaName}
                 />
 
                 {/* <ConfirmModal
@@ -284,12 +310,14 @@ const Role = React.createClass({
                     maskClosable={false} //设置不允许点击蒙层关闭
                     transitionName=""  //禁用modal的动画效果
                     footer={[
-                        <button type="primary" className="login-form-button examination_btn_blue calmSure" onClick={this.deleteModal}  >确定</button>,
-                        <button type="ghost" className="login-form-button examination_btn_white calmCancle" onClick={this.closeConfirmModal} >取消</button>
+                        <button type="primary" className="login-form-button examination_btn_blue calmSure"
+                                onClick={this.deleteModal}>确定</button>,
+                        <button type="ghost" className="login-form-button examination_btn_white calmCancle"
+                                onClick={this.closeConfirmModal}>取消</button>
                     ]}
                 >
                     <div className="isDel">
-                        <img className="sadFeel" src={require("../../../dist/jquery-photo-gallery/icon/sad.png")} />
+                        <img className="sadFeel" src={require("../../../dist/jquery-photo-gallery/icon/sad.png")}/>
                         确定删除?
                     </div>
                 </Modal>
