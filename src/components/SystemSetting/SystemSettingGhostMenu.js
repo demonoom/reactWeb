db21894d526e8d4f6e3a09b59cba4bdc6714ac26
noom <noom0130@foxmail.com>
2018-07-30 23:37:51 +0800

    .

--- src/components/SystemSetting/SystemSettingGhostMenu.js
+++ src/components/SystemSetting/SystemSettingGhostMenu.js
@@ -189,7 +189,7 @@ class SystemSettingGhostMenu extends React.Component {
                         _this.checkWords({
                             method: 'openNewPage',
                             // url: 'http://jiaoxue.maaee.com:8091/#/ARTextbookList'
-                             url: 'http://192.168.50.163:8091/#/ARTextbookList'
+                            url: 'http://192.168.50.29:8091/#/ARTextbookList'
 
                         }, 'AR教材');
                     }}><img className="icon_system_img" src="http://60.205.111.227/upload2/common/img/icon_ar.png"/>AR教材
