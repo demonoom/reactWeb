ba3a336cb37482d9f4a3a8a38e650d47944da0a1
calm <calm420@163.com>
2018-07-31 21:38:09 +0800

    .

--- src/components/SystemSetting/SystemSettingGhostMenu.js
+++ src/components/SystemSetting/SystemSettingGhostMenu.js
@@ -182,14 +182,13 @@ class SystemSettingGhostMenu extends React.Component {
             liArr = [];
         }
         if (_this.state.ident == 23836) {
-
             var newLi = <li className="multi">
                 <ul className="second">
                     <li onClick={event => {
                         _this.checkWords({
                             method: 'openNewPage',
-                            // url: 'http://jiaoxue.maaee.com:8091/#/ARTextbookList'
-                            url: 'http://192.168.50.72:8091/#/ARTextbookList'
+                            url: 'http://jiaoxue.maaee.com:8091/#/ARTextbookList'
+                            // url: 'http://192.168.50.72:8091/#/ARTextbookList'
                         }, 'AR教材');
                     }}><img className="icon_system_img" src="http://60.205.111.227/upload2/common/img/icon_ar.png"/>AR教材
                     </li>
@@ -198,16 +197,15 @@ class SystemSettingGhostMenu extends React.Component {
             liArr.push(newLi);
         }
         if (_this.state.ident == 23836) {
-
             var newLi = <li className="multi">
                 <ul className="second">
                     <li onClick={event => {
                         _this.checkWords({
                             method: 'openNewPage',
-                            // url: 'http://jiaoxue.maaee.com:8091/#/ARTextbookList'
-                            url: 'http://192.168.50.72:8091/#/ARTagList'
+                            url: 'http://jiaoxue.maaee.com:8091/#/ARTagList'
+                            // url: 'http://192.168.50.72:8091/#/ARTagList'
                         }, 'AR标签');
-                    }}><img className="icon_system_img" src="http://60.205.111.227/upload2/common/img/icon_ar.png"/>AR标签
+                    }}><img className="icon_system_img" src="http://60.205.111.227/upload2/common/img/icon_VRtag.png"/>AR标签
                     </li>
                 </ul>
             </li>;
@@ -219,7 +217,6 @@ class SystemSettingGhostMenu extends React.Component {
                 method: 'openNewPage',
                 url: 'http://jiaoxue.maaee.com:8091/#/classBrandTemplateList'
             }
-
             var newLi = <li className="multi">
                 <ul className="second">
                     <li onClick={event => {
