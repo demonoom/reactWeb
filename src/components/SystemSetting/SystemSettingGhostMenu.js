5b27f8dbd2ee95cef0bf2706e270c0038311468b
john_changliang <john_changliang@163.com>
2018-08-08 14:49:34 +0800

    给23836音乐上传的权限

--- src/components/SystemSetting/SystemSettingGhostMenu.js
+++ src/components/SystemSetting/SystemSettingGhostMenu.js
@@ -219,8 +219,8 @@ class SystemSettingGhostMenu extends React.Component {
                     <li onClick={event => {
                         _this.checkWords({
                             method: 'openNewPage',
-                            // url: 'http://jiaoxue.maaee.com:8091/#/uploadMusicList'
-                            url: 'http://192.168.50.72:8091/#/uploadMusicList'
+                            url: 'http://jiaoxue.maaee.com:8094/#/uploadMusicList'
+                            // url: 'http://192.168.50.72:8091/#/uploadMusicList'
                         }, '音乐上传');
                     }}><img className="icon_system_img" src="http://60.205.111.227/upload2/common/img/icon_ar.png"/>音乐上传
                     </li>
