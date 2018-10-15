b59512d125662c8b1f57e187880fca985b26dcb4
john_changliang <john_changliang@163.com>
2018-10-15 21:18:26 +0800

    课堂回顾参数使用vid

--- src/components/cloudClassRoom/AntMulitiClassComponents.js
+++ src/components/cloudClassRoom/AntMulitiClassComponents.js
@@ -947,7 +947,7 @@ const AntMulitiClassComponents = React.createClass({
         var newTab = window.open('about:blank');
         var requestUrl = FACE_EMOTIONS_URL;
 
-        requestUrl += "?vid="+liveObj.courseId+"&userId="+loginUser+"&type=2"+"&name="+encodeURI(liveObj.name);
+        requestUrl += "?vid="+liveObj.virtual_classId+"&userId="+loginUser+"&type=2"+"&name="+encodeURI(liveObj.name);
         //将之前打开的新窗口重定向到当前指定的路径上（目的：解决在ajax中打开新窗口被拦截的问题）
         newTab.location.href = requestUrl;
         
