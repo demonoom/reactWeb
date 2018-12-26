fda65bca5530a9484b3e168f6693481fcf168f30
王鹏飞 <398135290@qq.com>
2018-12-26 19:04:23 +0800

    --

--- src/utils/Const.js
+++ src/utils/Const.js
@@ -8,7 +8,7 @@ import { flattenDiagnosticMessageText } from "typescript";
 export const PAGE_SIZE = 30;
 export const CLOUD_CLASS_PAGE_SIZE = 20;
 export const ALL_TOPIC = 0;
-export const IS_DEBUG = true;   //是否是本地调试模式（true：使用本地测试地址；false：使用远程地址）
+export const IS_DEBUG = false;   //是否是本地调试模式（true：使用本地测试地址；false：使用远程地址）
 export const IS_LIVE_DEBUG = false;  //是否是直播调试模式（true：使用本地直播测试地址；false：使用本地webservice测试地址）
 export const ONLY_TEACHER_TOPIC = 1;
 export const TYPE_TEACHER = "TEAC";
